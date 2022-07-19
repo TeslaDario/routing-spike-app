ce7cf27aaf10ba145ab5a65354641a21e06f9036
Dario Dragisic <dado.dsa@gmail.com>
2022-07-19 12:44:50 +0200

    added ng material

---
+++ apps/routing/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgWelcomeComponent } from './ng-welcome.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+const routes: Routes = [
+  { path: 'ng', component: NgWelcomeComponent },
+  { path: 'nx', component: NxWelcomeComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- apps/routing/src/app/app.module.ts
+++ apps/routing/src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NgWelcomeComponent } from './ng-welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, NgWelcomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
